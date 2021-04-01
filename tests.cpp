bc5781a7d70123fb1ff3e44e339fd731026f5dd2
Xavrax <1i13i.dahlke@gmail.com>
2021-04-01 11:29:53 +0200

    create tests environment

---
+++ tests.cpp
@@ -0,0 +1,12 @@
+//#ifdef QUESTIONMARK_TEST
+
+#define CATCH_CONFIG_MAIN
+#include "external/catch2.hpp"
+
+namespace tests {
+    TEST_CASE("test", "") {
+
+    }
+}
+
+//#endif //QUESTIONMARK_TEST
