03fb299a7785368c2439cc20c67a879d373cf488
Xavrax <1i13i.dahlke@gmail.com>
2021-04-01 12:04:46 +0200

    add is_ok method

--- question_mark.hpp
+++ question_mark.hpp
@@ -1,7 +1,34 @@
 #ifndef QUESTIONMARK_HEADER
 #define QUESTIONMARK_HEADER
 
+#include <memory>
 
+template <typename T, typename E>
+class Result {
+public:
+    /// Creates successful result with some data
+    static Result Ok(T value) {
+        return Result(std::make_unique<T>(value), nullptr);
+    }
+
+    /// Creates result containing error
+    static Result Err(E error) {
+        return Result(nullptr, std::make_unique<E>(error));
+    }
+
+    /// Checks if result containing some data
+    bool is_ok() const {
+        return _ok != nullptr;
+    }
+
+    std::unique_ptr<T> _ok;
+    std::unique_ptr<E> _err;
+private:
+    Result(std::unique_ptr<T> ok, std::unique_ptr<E> err) {
+        this->_ok = std::move(ok);
+        this->_err = std::move(err);
+    }
+};
 
 
 
--- tests.cpp
+++ tests.cpp
@@ -1,8 +1,12 @@
 #define CATCH_CONFIG_MAIN
 #include "external/catch2.hpp"
+#include "question_mark.hpp"
 
 namespace tests {
-    TEST_CASE("test", "") {
-
+    TEST_CASE("check Result's methods", "[Result<T,E>]") {
+        SECTION("is ok") {
+            REQUIRE(Result<int, int>::Ok(10).is_ok());
+            REQUIRE_FALSE(Result<int, int>::Err(10).is_ok());
+        }
     }
 }
